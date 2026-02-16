8c6bb7334f6dff7ee52f22a8b63f15b8522f5cbe
Ayan Ali <ayanali20985@gmail.com>
2026-02-16 17:33:39 -0500

    fixes for the verialtor-bench stuff

--- verilator-view copy/obj_dir/genericVgaViewer.d
+++
@@ -1,6 +0,0 @@
-genericVgaViewer.o: ../genericVgaViewer.cpp VtbGenericVga.h \
- C:/msys64/ucrt64/share/verilator/include/verilated.h \
- C:/msys64/ucrt64/share/verilator/include/verilated_config.h \
- C:/msys64/ucrt64/share/verilator/include/verilatedos.h \
- C:/msys64/ucrt64/share/verilator/include/verilated_types.h \
- C:/msys64/ucrt64/share/verilator/include/verilated_funcs.h
